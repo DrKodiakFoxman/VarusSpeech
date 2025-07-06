0389ce51589be9e9ead62abadb0c8f150c3fa4e9
DrKodiakFoxman <167705495+DrKodiakFoxman@users.noreply.github.com>
2025-07-06 00:39:44 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-import os
-from flask import Flask, request, send_file, jsonify, render_template
-from tts import generate_tts
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    data = request.json
-    file_path = generate_tts(data['text'], data['voiceEn'], data['styleEn'], data['voiceEs'], data['styleEs'])
-    return send_file(file_path, as_attachment=False)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto en la variable de entorno PORT
-    app.run(host='0.0.0.0', port=port)
